7d382fd7daf5acee17ea5829de29f714036c630d
Shpend Mula <shmula@hotmail.com>
2021-03-29 19:53:59 +0200

    the ability to chain operations added

--- scripts/script.js
+++ scripts/script.js
@@ -11,6 +11,11 @@ let num2 = '';
 let mathOperation = "";
 let result = "";
 
+const toggleClearBtn = {
+    allClear: true,
+    clear: false
+}
+
 
 digitContainer.addEventListener('click',getOperands);
 operationsContainer.addEventListener('click',getOperation);
@@ -22,43 +27,74 @@ function displayResult(result) {
         resultCont.textContent = "ERR";
         return;
     } 
+    
     resultCont.textContent = result;
-    mathOperation = "";
-    num1 = '';
+    
+    num1 = ''; // you may not need these, trace code!
     num2 = '';
-    input = "";
+   
 }
 
 function updateDisplay(value){
     if(value.length > 8) {
         return;
     }
-    if(value.length === 1 && !num2 && !mathOperation) {
-        changeClear();
-    }
-    // if(value.length === 1 && num1 && mathOperation) {
-    //     changeClear();
-    // }
-   
+    // changeClear();    
     resultCont.textContent = "";
     resultCont.textContent += value;
 }
 
 function getOperation(ev) {
     if(ev.target.dataset.value === "=") {
-        getResult(num1,num2,mathOperation)
+        getResult(num1,num2,mathOperation);
+        num1 = "";
+        num2 = "";
     }
-    mathOperation = ev.target.dataset.value;
+    if(ev.target.dataset.value === "+" && mathOperation) {
+        getResult(num1,num2,mathOperation);
+        result = add(num1,num2);
+        displayResult(result);
+        num1 = "";
+        num2 = "";
+    }
+    if(ev.target.dataset.value === "-" && mathOperation) {
+        getResult(num1,num2,mathOperation);
+        result = substract(num1,num2);
+        displayResult(result);
+        num1 = "";
+        num2 = "";
+    }
+
+    if(ev.target.dataset.value === "*" && mathOperation) {
+        getResult(num1,num2,mathOperation);
+        if(num2 === "") num2 = 1;
+        result = multiply(num1,num2);
+        displayResult(result);
+        num1 = "";
+        num2 = "";
+    }
+
+    if(ev.target.dataset.value === "/" && mathOperation) {
+        getResult(num1,num2,mathOperation);
+        if(num2 === "") num2 = 1;
+        result = divide(num1,num2);
+        displayResult(result);
+        num1 = "";
+        num2 = "";
+    }
+    mathOperation = ev.target.dataset.value; 
 }
 
 function getOperands(ev) {
     if(!mathOperation) {
         num1 += ev.target.dataset.value;
         updateDisplay(num1);
+        
     } 
     if(mathOperation) {
         num2 += ev.target.dataset.value;
         updateDisplay(num2);
+        
     }   
 }
 
@@ -88,19 +124,35 @@ function getResult(num1,num2,operation) {
 
 
 function multiply(num1,num2) {
-    return num1 * num2;
+    if(result) {
+        return Number(result) * Number(num2);
+    } else {
+        return Number(num1) * Number(num2);
+    }
 }
 
 function add(num1,num2) {
-    return Number(num1) + Number(num2);
+    if(result) {
+        return Number(result) + Number(num2);
+    } else {
+        return Number(num1) + Number(num2);
+    }
 }
 
 function divide(num1,num2) {
-    return num1 / num2;
+    if(result) {
+        return Number(result) / Number(num2);
+    } else {
+        return Number(num1) / Number(num2);
+    }
 }
 
 function substract(num1,num2) {
-    return num1 - num2;
+    if(result) {
+        return Number(result) - Number(num2);
+    } else {
+        return Number(num1) - Number(num2);
+    }
 }
 
 function percentage(num1,num2) {
@@ -108,17 +160,32 @@ function percentage(num1,num2) {
     return decNum * num2;
 }
 
+// function changeClear() {  
+//     if(num1.length > 0 && !mathOperation && !num2) {
+//         toggleClearBtn.allClear = false;
+//         toggleClearBtn.clear = true;
+//         clearAllBtn.style.display = "none";
+//         clearBtn.style.display = "block";
+//         console.log("hello");
+//     }
+//     if(num1 && mathOperation && !num2) {
+//         toggleClearBtn.allClear = true;
+//         toggleClearBtn.clear = false;
+//         clearAllBtn.style.display = "block";
+//         clearBtn.style.display = "none";
+//         console.log("hi")
+//     } 
+// }
 
-
-function changeClear() {
-    if(window.getComputedStyle(clearAllBtn,null).display === 'block') {
-        clearAllBtn.style.display = "none";
-        clearBtn.style.display = "block";
-    } else {
-        clearAllBtn.style.display = "block";
-        clearBtn.style.display = "none"; 
-    } 
-}
+// function changeClear() {
+//     if(window.getComputedStyle(clearAllBtn,null).display === 'block') {
+//         clearAllBtn.style.display = "none";
+//         clearBtn.style.display = "block";
+//     } else {
+//         clearAllBtn.style.display = "block";
+//         clearBtn.style.display = "none"; 
+//     } 
+// }s
     
 
 
@@ -126,16 +193,16 @@ function clear() {
     if(num1 && !mathOperation && !num2) {
         num1 = "";
         resultCont.textContent = "0";
-        changeClear();
+        // changeClear();
     }
     if(num1 && mathOperation && !num2) {
         mathOperation = "";
-        changeClear();
+        // changeClear();
     }
     if(num1 && mathOperation && num2) {
         num2 = "";
         resultCont.textContent = "0";
-        changeClear();
+        // changeClear();
     }
     // if(num1 && mathOperation && num2 && result) {
     //     console.log('hello')
